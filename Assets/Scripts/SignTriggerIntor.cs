0c57005c5bc26cbf7617868bdbc2383852ff2eb3
Valdarix <jwest@satori-assoc.com>
2021-05-02 19:13:25 -0400

    push files
    INit push

---
+++ Assets/Scripts/SignTriggerIntor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignTriggerIntor : MonoBehaviour {
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Sign Collision Triggered");
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Sign Collision Triggered by player");
+
+            //make the player bounce off the enemy aka jump
+            other.gameObject.GetComponent<CharacterController2D>().FallDeath();
+        }
+    }
+}
